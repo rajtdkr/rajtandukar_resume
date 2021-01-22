d5a8abc15c03754db31bfd6dfbf303d789ca3b2b
Raj <raj@laconik.com.au>
2021-01-22 16:42:38 +0800

    Portfolio completed

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Card } from 'antd';
-import { Divider } from 'antd';
+import { Divider } from '@material-ui/core';
 
 
 class Portfolio extends Component {
@@ -8,39 +8,49 @@ class Portfolio extends Component {
 
   render() {
       const mystyle = {
-          width: 600,
+          minWidth: 1000,
           padding:30,
-          background: '#ffffff'
+          background: '#ffffff',
+          textAlign : 'left'
       }
       const gridStyle = {
           width: '25%',
-          textAlign: 'center',
         };
+      const mystyle_cards={width : 200,
+        }
 
     if(this.props.data){
       var projects = this.props.data.projects.map(function(projects){
         var projectImage = 'images/portfolio/'+ projects.image;
-        return <div className="site-card-border-less-wrapper">
-           <Card  style={mystyle} bordered={true}>
-                <Divider dashed > <br /> </Divider>
-               <p>
-               <h5>{projects.title}</h5>
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-                </a></p>
-        </Card>
 
+        return <div style={{width : '500'}}>
+
+           <Card  style={mystyle} bordered={true}>
+        <div style={{width: '100%', overflow: 'hidden'}}>
+               <h5>{projects.title}</h5>
+               <a href={projects.url} title={projects.title} >
+                   <div style={{width: '300px', float: 'left'}}>
+               <img alt={projects.title} src={projectImage} style={mystyle_cards}/>
+                   </div>
+
+
+                   <div style={{marginLeft: '20px', textColor : '#ffffff'}}>
+                       
+                       <h6>{projects.description}</h6></div>
+                </a>
+            </div>
+        </Card>
+            <br/>
         </div>
       })
     }
     return (
       <section id="portfolio">
-      <div className="row">
+      <div className="row" style={{width : '2000px'}}>
          <div className="twelve columns collapsed">
             <h1>Check Out Some of My Works.</h1>
             <div >
                 {projects}
-
             </div>
           </div>
       </div>
